4ce3c35a5510662b0a71dbc2e6d8d23e5c9a9e8f
Cody <cody.tian@icloud.com>
2021-12-25 15:55:32 -0800

    add highlightList, msPerPassage

--- src/metroidNetworkStation.java
+++ src/metroidNetworkStation.java
@@ -2,6 +2,9 @@ import peasy.PeasyCam;
 import processing.core.PApplet;
 import processing.data.JSONArray;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+
 
 /**
  * Thanks to our hero Abe Pazos at https://vimeo.com/channels/p5idea, who
@@ -11,9 +14,9 @@ public class metroidNetworkStation extends PApplet {
 	// our list of passages
 	String[] textList = new String[6];
 	// our highlight list, keeping track of which indices to highlight
-	int[][] highlightList;
+	ArrayList<int[][]> highlightList = new ArrayList<>();
 	// a list keeping track of the milliseconds per each passage
-	int[] msPerPassage;
+	int[] msPerPassage = new int[6];
 	// our usual camera for 3D visualizations
 	PeasyCam cam;
 	public static void main(String[] args) {
@@ -41,7 +44,20 @@ public class metroidNetworkStation extends PApplet {
 //		System.out.println(json);
 		for (int i = 0; i < json.size(); i++) {
 			textList[i] = json.getJSONObject(i).getString("text");
-			System.out.println(json.getJSONObject(i).getString("text"));
+			msPerPassage[i] = json.getJSONObject(i).getInt("ms");
+			int[][] lst = new int[json.getJSONObject(i).size()-1][2];
+			for (int j = 0; j < json.getJSONObject(i).size(); j++) {
+				JSONArray highlights = json.getJSONObject(i).getJSONArray(
+						"highlightIndices");
+				for (int k = 0; k < highlights.size(); k++) {
+					lst[k][0] = highlights.getJSONObject(k).getInt("start");
+					lst[k][1] = highlights.getJSONObject(k).getInt("end");
+				}
+			}
+			highlightList.add(lst);
+		}
+		for (int[][] h : highlightList) {
+			System.out.println(Arrays.deepToString(h));
 		}
 	}
 
